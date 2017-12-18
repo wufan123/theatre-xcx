b388974ab1547c97bb0e096b73947557b3be8b76
林傅荣 <459558744@qq.com>
2017-12-18 17:38:11 +0800

    购票模块布局

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,17 @@
 //获取应用实例
 Page({
   data: {
+    goodsList: {
+      ticketShow: [
+        {id: "123"},{id: "456"},{id: "789"},{id: "456"},{id: "789"}
+      ],
+      ticketSet: [
+        {id: "123"},{id: "456"},{id: "789"}
+      ],
+      ticketVoucher: [
+        {id: "123"},{id: "456"},{id: "789"}
+      ]
+    }
   },
   onLoad: function (e) {
   }
