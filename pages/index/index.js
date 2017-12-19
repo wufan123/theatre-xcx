1a4b1c898b02b7cbea4340c2f7d30656b9261dc3
林傅荣 <459558744@qq.com>
2017-12-19 09:13:38 +0800

    fix

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,8 +3,7 @@ Page({
   data: {
     bannerList: {
       list: [
-        {url: "http://pic23.photophoto.cn/20120530/0020033092420808_b.jpg"},
-        {url: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1513600848314&di=a984ece5f71542b11a0e57386422cb87&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimage%2Fc0%253Dshijue1%252C0%252C0%252C294%252C40%2Fsign%3Dcfb53f93c3177f3e0439f44e18a651b2%2F6609c93d70cf3bc7814060c9db00baa1cd112a56.jpg"}
+        {url: "http://pic23.photophoto.cn/20120530/0020033092420808_b.jpg"}
       ]
     },
     goodsList: {
