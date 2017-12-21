aa18e028551549c4c33d3f4b0fdd70b4a293d865
林傅荣 <459558744@qq.com>
2017-12-21 15:33:18 +0800

    卖品流程

--- pages/ticket/goods/goods.js
+++ pages/ticket/goods/goods.js
@@ -14,6 +14,28 @@ Page({
             console.log(res)
         })
     },
+    addQuatity: function (e) {
+        var goods = this.data.goodsList[e.currentTarget.dataset.index];
+        if (!goods) return;
+        if (!goods.num) goods.num = 0;
+        goods.num++;
+        this.caculateCount()
+    },
+    subtractQuatity: function (e) {
+        var goods = this.data.goodsList[e.currentTarget.dataset.index];
+        if (!goods || !goods.num) return;
+        goods.num--;
+        this.caculateCount()
+    },
+    caculateCount: function () {
+        var count = 0;
+        for (var i = 0; i < this.data.goodsList.length; i++) {
+            var item = this.data.goodsList[i];
+            if (item.num) count += item.num * item.price;
+        }
+        this.data.bottomTxt = count>0?"下一步（￥"+count+"）":"不选了，直接下单购票"
+        this.setData(this.data);
+    },
     confirm: function() {
         wx.redirectTo({
             url: '../confirm/confirm',
