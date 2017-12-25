2fbed2180fa86b829057e6f2fb19e3784b5ef556
林傅荣 <459558744@qq.com>
2017-12-25 17:11:56 +0800

    绑定排期界面

--- pages/ticket/index/index.js
+++ pages/ticket/index/index.js
@@ -12,12 +12,14 @@ Page({
         filmPlan: {
             timeList: [],
             planList: [],
+            timeSelected: null,
+            planSelected: null,
             count: 0
         },
         bottomTxt: '马上购买'
     },
     onLoad: function (e) {
-        this.loadFilmDetail();
+        this.loadFilmTime();
     },
     confirm: function(e) {
         if (!this.data.filmDetail.showPlan) {
@@ -46,72 +48,65 @@ Page({
     emptyBtn: function() {
 
     },
-    // 加载影片详情
-    loadFilmDetail: function() {
-        modalUtil.showLoadingToast()
-        filmRest.getMove(1, app.globalData.cinemaCode, res => {
-            if (res.length > 0) {
-                filmRest.getFilmDetail(res[0].id, res => {
-                    modalUtil.hideLoadingToast();
-                    res.introduction = res.introduction.replace('&lt;html&gt;&lt;body&gt;', '')
-                                                        .replace('&lt;/body&gt;&lt;/html&gt;', '')
-                                                        .replace(/&lt;/g, "<")
-                                                        .replace(/&gt;/g, ">")
-                                                        .replace(/&amp;/g, "&")
-                                                        .replace(/&quot;/g, '"')
-                                                        .replace(/&apos;/g, "'")
-                                                        .replace(/\<br\s*\/\>/g, "\r\n")
-                    this.data.filmDetail.detail = res
-                    this.setData(this.data)
-                    this.loadFilmTime();
-                }, res => {
-                    console.log(res)
-                    modalUtil.hideLoadingToast();
-                    modalUtil.showWarnToast("影片加载失败");
-                });
-            } else {
-                modalUtil.hideLoadingToast();
-                modalUtil.showWarnToast("影片加载失败");
-            }
-        }, res => {
-            console.log(res)
-            modalUtil.hideLoadingToast();
-            modalUtil.showWarnToast("影片加载失败");
-        });
-    },
+    // 营业日期
     loadFilmTime: function() {
         let params = {
-            filmNo: this.data.filmDetail.detail.filmNo,
             cinemaCode: app.globalData.cinemaCode
         }
         planRest.getTimes(params, res => {
             this.data.filmPlan.timeList = res;
             this.setData(this.data)
             if (res.length > 0) {
+                this.data.filmPlan.timeSelected = res[0].time
                 this.loadFilmPlan(res[0].time);
             }
         }, res => {
             console.log(res)
         })
     },
+    // 排期
     loadFilmPlan: function(time) {
         let params = {
-            filmNo: this.data.filmDetail.detail.filmNo,
             cinemaCode: app.globalData.cinemaCode,
             time: time
         }
         planRest.getPlans(params, res => {
-            if (res.planInfo && res.planInfo.length>0) {
-                this.data.filmPlan.planList = res.planInfo;
-                this.data.filmDetail.standardPrice = res.planInfo[0].standardPrice // 取价格
+            if (res.length>0) {
+                if (!this.data.filmDetail.detail) {
+                    this.loadFilmDetail(res[0].filmId)
+                }
+                this.data.filmPlan.planList = res[0].planInfo;
+                this.data.filmDetail.standardPrice = res[0].planInfo[0].standardPrice // 取价格
+                this.data.filmPlan.planSelected = res[0].planInfo[0].featureAppNo
                 this.setData(this.data)
             }
         }, res => {
             console.log(res)
         })
     },
+    // 加载影片详情
+    loadFilmDetail: function(filmId) {
+        modalUtil.showLoadingToast()
+        filmRest.getFilmDetail(filmId, res => {
+            modalUtil.hideLoadingToast();
+            res.introduction = res.introduction.replace('&lt;html&gt;&lt;body&gt;', '')
+                                                .replace('&lt;/body&gt;&lt;/html&gt;', '')
+                                                .replace(/&lt;/g, "<")
+                                                .replace(/&gt;/g, ">")
+                                                .replace(/&amp;/g, "&")
+                                                .replace(/&quot;/g, '"')
+                                                .replace(/&apos;/g, "'")
+                                                .replace(/\<br\s*\/\>/g, "\r\n")
+            this.data.filmDetail.detail = res
+            this.setData(this.data)
+        }, res => {
+            console.log(res)
+            modalUtil.hideLoadingToast();
+            modalUtil.showWarnToast("影片加载失败");
+        });
+    },
 
-    // 排期选择
+    // 数量
     subtract: function () {
         this.data.filmPlan.count = this.data.filmPlan.count - 1;
         this.data.filmPlan.count = this.data.filmPlan.count > 0 ? this.data.filmPlan.count : 0
@@ -120,5 +115,17 @@ Page({
     add: function () {
         this.data.filmPlan.count = Number(this.data.filmPlan.count) + 1
         this.setData(this.data)
+    },
+    // 营业日期选择
+    selectTime: function(e) {
+        this.data.filmPlan.timeSelected = e.currentTarget.id
+        this.setData(this.data)
+        // TODO
+    },
+    // 排期选择
+    selectPlan: function(e) {
+        this.data.filmPlan.planSelected = e.currentTarget.id
+        this.setData(this.data)
+        // TODO 
     }
 })
