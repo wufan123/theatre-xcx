7f23148ab7cec66c591d252f723858d152e688f4
林傅荣 <459558744@qq.com>
2017-12-25 16:46:07 +0800

    商品详情

--- pages/business/goodDetail/goodDetail.js
+++ pages/business/goodDetail/goodDetail.js
@@ -5,126 +5,22 @@ const modalUtil = require('../../../util/modalUtil.js')
 var app = getApp();
 Page({
   data: {
-    filmDetail: {
-      detail: null,
-      standardPrice: 0,
-      showPlan: false
-    },
-    filmPlan: {
-      timeList: [],
-      planList: [],
-      count: 0
+    goodsDetail: {
+      detail: null
     },
     bottomTxt: '马上购买'
   },
   onLoad: function (e) {
-    console.log(e)
     storeRest.getGoodsDetail(e.goodsId, success => {
-      console.log(success)
+      this.data.goodsDetail.detail = success.goodInfo
+      this.setData(this.data)
     }, error => {
       console.log(error)
     })
   },
   confirm: function (e) {
-    if (!this.data.filmDetail.showPlan) {
-      this.data.filmDetail.showPlan = true;
-      this.data.bottomTxt = '确定'
-      this.setData(this.data)
-    } else {
-      // TODO 判断参数
-      wx.navigateTo({
-        url: '../goods/goods',
-      })
-
-      // 还原界面状态
-      setTimeout(() => {
-        this.data.filmDetail.showPlan = false;
-        this.data.bottomTxt = '马上购买'
-        this.setData(this.data)
-      }, 1000)
-    }
-  },
-  hidePlan: function () {
-    this.data.filmDetail.showPlan = false;
-    this.data.bottomTxt = '马上购买'
-    this.setData(this.data)
-  },
-  emptyBtn: function () {
-
-  },
-  // 加载影片详情
-  loadFilmDetail: function () {
-    modalUtil.showLoadingToast()
-    filmRest.getMove(1, app.globalData.cinemaCode, res => {
-      if (res.length > 0) {
-        filmRest.getFilmDetail(res[0].id, res => {
-          modalUtil.hideLoadingToast();
-          res.introduction = res.introduction.replace('&lt;html&gt;&lt;body&gt;', '')
-            .replace('&lt;/body&gt;&lt;/html&gt;', '')
-            .replace(/&lt;/g, "<")
-            .replace(/&gt;/g, ">")
-            .replace(/&amp;/g, "&")
-            .replace(/&quot;/g, '"')
-            .replace(/&apos;/g, "'")
-            .replace(/\<br\s*\/\>/g, "\r\n")
-          this.data.filmDetail.detail = res
-          this.setData(this.data)
-          this.loadFilmTime();
-        }, res => {
-          console.log(res)
-          modalUtil.hideLoadingToast();
-          modalUtil.showWarnToast("影片加载失败");
-        });
-      } else {
-        modalUtil.hideLoadingToast();
-        modalUtil.showWarnToast("影片加载失败");
-      }
-    }, res => {
-      console.log(res)
-      modalUtil.hideLoadingToast();
-      modalUtil.showWarnToast("影片加载失败");
-    });
-  },
-  loadFilmTime: function () {
-    let params = {
-      filmNo: this.data.filmDetail.detail.filmNo,
-      cinemaCode: app.globalData.cinemaCode
-    }
-    planRest.getTimes(params, res => {
-      this.data.filmPlan.timeList = res;
-      this.setData(this.data)
-      if (res.length > 0) {
-        this.loadFilmPlan(res[0].time);
-      }
-    }, res => {
-      console.log(res)
-    })
-  },
-  loadFilmPlan: function (time) {
-    let params = {
-      filmNo: this.data.filmDetail.detail.filmNo,
-      cinemaCode: app.globalData.cinemaCode,
-      time: time
-    }
-    planRest.getPlans(params, res => {
-      if (res.planInfo && res.planInfo.length > 0) {
-        this.data.filmPlan.planList = res.planInfo;
-        this.data.filmDetail.standardPrice = res.planInfo[0].standardPrice // 取价格
-        this.setData(this.data)
-      }
-    }, res => {
-      console.log(res)
-    })
-  },
-
-  // 排期选择
-  subtract: function () {
-    this.data.filmPlan.count = this.data.filmPlan.count - 1;
-    this.data.filmPlan.count = this.data.filmPlan.count > 0 ? this.data.filmPlan.count : 0
-    this.setData(this.data)
-  },
-  add: function () {
-    this.data.filmPlan.count = Number(this.data.filmPlan.count) + 1
-    this.setData(this.data)
+    // wx.navigateTo({
+    //   url: '../goods/goods',
+    // })
   }
 })
