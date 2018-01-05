a5ea3915f6e1ac7fedb5affe707d08e948d3321e
林傅荣 <459558744@qq.com>
2018-01-05 10:35:39 +0800

    分享页面配置

--- pages/me/share/share.js
+++ pages/me/share/share.js
@@ -1,11 +1,32 @@
+const theatreRest = require('../../../rest/theatreRest')
 var app = getApp()
 
 Page({
   data: {
-    isShare:false
+    isShare:false,
+    titleConfig: '',
+    subtitleConfig: '',
+    ruleConfig: ''
   },
   onLoad: function (options) {
-  
+    theatreRest.getMiscConfig('invite_reg_title', success => {
+      if (success && success.length > 0) {
+        this.data.titleConfig = success[0].miscVal
+        this.setData(this.data)
+      }
+    })
+    theatreRest.getMiscConfig('invite_reg_subtitle', success => {
+      if (success && success.length > 0) {
+        this.data.subtitleConfig = success[0].miscVal
+        this.setData(this.data)
+      }
+    })
+    theatreRest.getMiscConfig('invite_reg_rule', success => {
+      if (success && success.length > 0) {
+        this.data.ruleConfig = success[0].miscVal
+        this.setData(this.data)
+      }
+    })
   },
   goShare:function(){
     this.setData({
